9d42c39c358593ed50f3540308f1ff78b3e54354
truongjaenokia <truongjae@gmail.com>
2022-06-09 23:08:09 +0700

    update

---
+++ prj16nangluongmattroi/sketch_apr29a/sketch_apr29a.ino
@@ -0,0 +1,39 @@
+#include<LiquidCrystal.h>
+LiquidCrystal lcd(11, 10, 5, 4, 3, 2);
+
+const int currentPin = A0;
+int sensitivity = 66;
+int adcValue = 0;
+int offsetVoltage = 0;
+double adcVoltage = 0;
+double currentValue = 0;
+
+void setup() {
+  lcd.begin(16, 2);
+}
+
+void loop() {
+  adcValue = analogRead(currentPin); // 0 - 1024
+  adcVoltage = (adcValue / 1024.0) * 5000;
+  currentValue = ( (adcVoltage - offsetVoltage) / sensitivity );
+  int loadVoltage = currentValue * 12;
+  int power = loadVoltage * currentValue * currentValue;
+
+  lcd.print("Current = ");
+  lcd.print(currentValue, 2);
+  lcd.print(" A");
+  lcd.setCursor(0, 1);
+  lcd.print("Voltage = ");
+  lcd.print(loadVoltage);
+  lcd.print(" V");
+  lcd.setCursor(0, 0);
+
+  delay(800);
+  lcd.print("Power = ");
+  lcd.print(power);
+  lcd.print(" Watt        ");
+  lcd.setCursor(0, 1);
+  lcd.print("                       ");
+  lcd.setCursor(0, 0);
+  delay(1000);
+}
